3754ad646707284460f41e70bebbd996c7d5268e
Carlos Kazuo <ninetails.nine@gmail.com>
2018-04-16 02:38:07 -0300

    Done server part

---
+++ .build/main.a7b7290c353b56d1ec8c.hot-update.js
@@ -0,0 +1,16 @@
+exports.id = "main";
+exports.modules = {
+
+/***/ "./server/server.js":
+/*!**************************!*\
+  !*** ./server/server.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! express */ \"express\");\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_0__);\n\nvar app = express__WEBPACK_IMPORTED_MODULE_0___default()();\napp.get('/api', function (req, res) {\n  res.send({\n    message: 'I am a server route and can also be hot reloaded!'\n  });\n});\n/* harmony default export */ __webpack_exports__[\"default\"] = (app);\n\n//# sourceURL=webpack:///./server/server.js?");
+
+/***/ })
+
+};
